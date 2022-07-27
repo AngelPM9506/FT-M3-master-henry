5bba6a0b69341450bc2129d601629bfe0eb03c4a
AngelPM9506 <parra_mondragon@live.com>
2022-07-27 10:09:56 -0500

    Tercera tarea terminada con exito solo faltarian los estilos

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,80 @@
+/**dependencias */
 var http = require('http');
-var fs   = require('fs');
+var fs = require('fs');
+const url = require('url');
 
-var beatles=[{
-  name: "John Lennon",
-  birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
-},
-{
-  name: "Paul McCartney",
-  birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
-},
-{
-  name: "George Harrison",
-  birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
-}
+
+/**datos */
+var beatles = [
+  {
+    name: "John Lennon",
+    birthdate: "09/10/1940",
+    profilePic: "https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
+  },
+  {
+    name: "Paul McCartney",
+    birthdate: "18/06/1942",
+    profilePic: "http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
+  },
+  {
+    name: "George Harrison",
+    birthdate: "25/02/1946",
+    profilePic: "https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
+  },
+  {
+    name: "Richard Starkey",
+    birthdate: "07/08/1940",
+    profilePic: "http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+  }
 ]
+/**parametros*/
+const host = '127.0.0.1';
+const port = 8080;
+
+/**responce */
+const listener = (req, res) => {
+  var status = 0;
+  var templeteHTML = fs.readFileSync(`${__dirname}/index.html`, 'utf-8');
+  if (req.url === '/') {
+    status = 200;
+    let beatlesHTMLArray = beatles.map(beatle => {
+      let html = fs.readFileSync(`${__dirname}/beatle.html`, 'utf-8');
+      html = html.replace(/{name}/g, beatle.name);
+      html = html.replace(/{birthdate}/g, beatle.birthdate);
+      html = html.replace(/{profilePic}/g, beatle.profilePic);
+      return html;
+    });
+    console.log(beatlesHTMLArray);
+    templeteHTML = templeteHTML.replace('{section}', beatlesHTMLArray.join('\n'));
+    res.writeHead(status, { 'Content-Type': 'text/html' });
+    res.end(templeteHTML);
+  } else {
+    console.log(url.parse(req.url, true).query.name);
+    for (let i = 0; i < beatles.length; i++) {
+      if (url.parse(req.url, true).query.name === beatles[i].name) {
+        status = 200;
+        let html = fs.readFileSync(`${__dirname}/beatle.html`, 'utf-8');
+        html = html.replace(/{name}/g, beatles[i].name);
+        html = html.replace(/{birthdate}/g, beatles[i].birthdate);
+        html = html.replace(/{profilePic}/g, beatles[i].profilePic);
+        templeteHTML = templeteHTML.replace('{section}', html);
+        res.writeHead(status, { 'Content-Type': 'text/html' });
+        res.end(templeteHTML);
+        return;
+      }
+    }
+    status = 404;
+    res.writeHead(status, { 'Content-Type': 'text/html' });
+    res.end(`<h1>${status}</h1><p>No se encontro ningun beatle con ese nombre</p>`);
+  }
+}
+
+const server = http.createServer(listener);
+server.listen(port, host, () => {
+  console.log(`Server Runing at ${host}:${port}`);
+})
+
+/**
+ * templeteHTML.replace('{section}', html) solo reemplaza una vez lo qeu este entre las comillas simples 
+ * html.replace(/{name}/g, beatles[i].name); rempleza todas las veces que aparece lo que esta entre las diagonales
+ */
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,38 @@
-var fs  = require("fs")
-var http  = require("http")
+/**modulos requeridos */
+var fs = require("fs");
+var http = require("http");
+var url = require("url");
 
 // Escribí acá tu servidor
+
+/**parametros**/
+const host = '127.0.0.1';
+const port = 8080;
+
+/**responses */
+const requestListener = (req, res) => {
+    let status = 0;
+    let image = url.parse(req.url, true).query;
+    let pathFile = __dirname + `/images/${image.name}`;
+    if (fs.existsSync(pathFile)) {
+        status = 202;
+        let file = fs.readFileSync(pathFile);
+        res.writeHead(status, { 'Content-Type': 'image/jpeg' });
+        res.end(file);
+    } else {
+        status = 404;
+        let error = {
+            status: status,
+            message: 'No se encontro ninguna imagen con ese nombre'
+        }
+        res.writeHead(status, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(error));
+    }
+    console.log(`${status} ${req.url}`);
+}
+
+/**servidor como tal */
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Servidor corriendo en: ${host}:${port}`);
+})
